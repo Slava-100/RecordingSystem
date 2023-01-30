ef2b761ba33f6df5ae57c64de41d22ec12e72f8e
Svyatoslav <svat.strel.2001@gmail.com>
2023-01-30 21:54:25 +0300

    Change program file

--- RecordingSystem.DB/TestConsole/Program.cs
+++ RecordingSystem.DB/TestConsole/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using RecordingSystem.DAL.Repositories;
+
+var d = new PatientRepository();
+d.AddPatient("Vova", "Str", "123-123", "@", null, true, "01-12-2012");
+
+
+//var p = new DoctorRepository();
+//var ppp = p.GetAllDoctors();
+Console.WriteLine("qwe");
