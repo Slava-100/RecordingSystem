9a3e6f8909b91c3af2b3b7970c77ccc66d33d063
andreymuskat <andrey.strel.2001@gmail.com>
2023-01-30 22:39:34 +0300

    Change procedure GetAllDoctors

--- RecordingSystem.DB/RecordingSystem.DB/dbo/Stored Procedures/GetAllDoctors.sql
+++ RecordingSystem.DB/RecordingSystem.DB/dbo/Stored Procedures/GetAllDoctors.sql
@@ -1,3 +1,5 @@
 ﻿CREATE PROCEDURE [dbo].[GetAllDoctors]
 AS
-select Doctor.Name , Doctor.LastName , Doctor.Id from dbo.[Doctor]
+select D.Id , D.Name , D.LastName , Sp.Name , Cab.Number AS NumberCabinet from dbo.[Doctor] AS D
+LEFT JOIN dbo.Specialization AS Sp ON D.[SpecializationId] = Sp.[Id]
+LEFT JOIN dbo.Cabinet AS Cab ON  D.[CabinetId] = Cab.[Id]
