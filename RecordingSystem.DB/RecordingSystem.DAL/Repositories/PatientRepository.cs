7a59aaba7a381ae47c2ac71fb7f4567f72e39f7b
Svyatoslav <svat.strel.2001@gmail.com>
2023-01-30 21:21:52 +0300

    Change StoredProcedure

--- RecordingSystem.DB/RecordingSystem.DAL/Repositories/PatientRepository.cs
+++ RecordingSystem.DB/RecordingSystem.DAL/Repositories/PatientRepository.cs
@@ -1,12 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using RecordingSystem.DAL.Models;
+using Dapper;
+using Microsoft.Data.SqlClient;
+using System.Data;
 
 namespace RecordingSystem.DAL.Repositories
 {
-    internal class PatientRepository
+    public class PatientRepository
     {
+        public void AddPatient(string name, string lastName, string phoneNumber, string email, int? statusId, bool male, string birthday)
+        {
+            using (var sqlConnection = new SqlConnection(Options.sqlConnection))
+            {
+                sqlConnection.Open();
+
+                sqlConnection.Query(StoredNamesProcedures.AddPatient,
+                    new { name, lastName, phoneNumber, email, statusId, male, birthday },
+                    commandType: CommandType.StoredProcedure).ToList();
+            }
+        }
     }
 }
