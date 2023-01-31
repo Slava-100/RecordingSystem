703007517cbdd39f2263dcb66f3b5eb97ea73a71
Svyatoslav <svat.strel.2001@gmail.com>
2023-02-01 00:05:57 +0300

    Add procedure UpdatePatientById

---
+++ RecordingSystem.DB/RecordingSystem.DB/dbo/Stored Procedures/UpdatePatientById.sql
@@ -0,0 +1,22 @@
+﻿CREATE PROCEDURE [dbo].[UpdatePatientById]
+	@Id INT,
+	@Name NVARCHAR(30),
+	@LastName NVARCHAR(30),
+	@PhoneNumber NVARCHAR(10),
+	@Email NVARCHAR(30),
+	@StatusId Int,
+	@Male bit,
+	@IsDeleted bit,
+	@Birthday datetime2(7)
+AS
+	UPDATE Patient
+	SET
+	[Name] = @Name,
+	[LastName] = @LastName,
+	[PhoneNumber] = @PhoneNumber,
+	[Email] = @Email,
+	[StatusId] = @StatusId,
+	[Male] = @Male,
+	[IsDeleted] = @IsDeleted,
+	[Birthday] = @Birthday
+	WHERE [Id]=@Id
