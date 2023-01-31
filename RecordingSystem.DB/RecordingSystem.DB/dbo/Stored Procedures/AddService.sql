3e88c630811410d10392e1f892163ca1ea9d77f2
andreymuskat <andrey.strel.2001@gmail.com>
2023-01-31 17:20:27 +0300

    Add new procedure AddService

---
+++ RecordingSystem.DB/RecordingSystem.DB/dbo/Stored Procedures/AddService.sql
@@ -0,0 +1,23 @@
+﻿CREATE PROCEDURE [dbo].[AddService]
+	@Name nvarchar(50),
+	@ServiceCategory nvarchar(50),
+	@Price float,
+	@SpecializationId int,
+	@Male bit
+AS
+INSERT INTO dbo.[Service]
+(
+	Name ,
+	ServiceCategory ,
+	Price ,
+	SpecializationId ,
+	Male 
+)
+VALUES
+(
+	@Name ,
+	@ServiceCategory ,
+	@Price ,
+	@SpecializationId ,
+	@Male
+)
