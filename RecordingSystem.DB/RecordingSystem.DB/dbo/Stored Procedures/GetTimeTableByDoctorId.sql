712a240951e7fafd191095a59ff1a4cc6234b394
andreymuskat <andrey.strel.2001@gmail.com>
2023-01-31 15:45:15 +0300

    Little change

--- RecordingSystem.DB/RecordingSystem.DB/dbo/Stored Procedures/GetTimeTableByDoctorId.sql
+++ RecordingSystem.DB/RecordingSystem.DB/dbo/Stored Procedures/GetTimeTableByDoctorId.sql
@@ -1,7 +1,7 @@
 ﻿CREATE PROCEDURE [dbo].[GetTimeTableByDoctorId]
 @Id int
 AS
-select D.Id , D.Name , D.LastName , Sp.Name AS Specialization , Cab.Number AS NumberCabinet , DW.Name AS DayofWeek , TS.Start , TS.[End] from dbo.[Doctor] AS D
+select D.Id , D.Name , D.LastName ,Sp.Id As SpecializationId , Sp.Name AS Specialization , Cab.Number AS NumberCabinet , DW.Name AS DayofWeek , TS.Start , TS.[End] from dbo.[Doctor] AS D
 LEFT JOIN dbo.Specialization AS Sp ON D.[SpecializationId] = Sp.[Id]
 LEFT JOIN dbo.Cabinet AS Cab ON  D.[CabinetId] = Cab.[Id]
 LEFT JOIN dbo.TimeTable AS TT ON  D.[Id] = TT.[DoctorId]
