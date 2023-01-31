5bfbb11da9ebcbbf30a92015c24ef9756a9a3b03
Svyatoslav <svat.strel.2001@gmail.com>
2023-01-31 23:33:37 +0300

    Add procedure UpdateService

---
+++ RecordingSystem.DB/RecordingSystem.DB/dbo/Stored Procedures/UpdateServiceById.sql
@@ -0,0 +1,16 @@
+﻿CREATE PROCEDURE [dbo].[UpdateServiceById]
+	@Id INT,
+	@Name NVARCHAR(50),
+	@Price FLOAT,
+	@SpecializationId int,
+	@IsDeleted bit,
+	@Male bit
+AS
+	UPDATE Service
+	SET
+	[Name] = @Name,
+	[Price] = @Price,
+	[SpecializationId] = @SpecializationId,
+	[IsDeleted] = @IsDeleted,
+	[Male] = @Male
+	WHERE [Id]=@Id
