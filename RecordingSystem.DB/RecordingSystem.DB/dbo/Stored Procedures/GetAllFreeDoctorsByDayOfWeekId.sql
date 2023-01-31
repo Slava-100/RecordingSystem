fd4cffa621102d0f4f1b9e4d2ab1d6d36cc5c134
Svyatoslav <svat.strel.2001@gmail.com>
2023-01-31 18:36:17 +0300

    Add new procedure

---
+++ RecordingSystem.DB/RecordingSystem.DB/dbo/Stored Procedures/GetAllFreeDoctorsByDayOfWeekId.sql
@@ -0,0 +1,10 @@
+﻿create procedure GetAllFreeDoctorsByDayOfWeekId
+@Id_DayOfWeek Int
+AS
+select Da.Id, Da.Name AS DayOfWeekName, D.Id, D.Name AS DoctorName, D.LastName, TT.Id, TS.Start, TS.[End], TR.Id, TR.[Date], TR.Occupied
+FROM dbo.[DayOfWeek] AS Da
+Left Join dbo.TimeTable AS TT ON Da.Id = TT.DayOfWeekId
+Left Join dbo.TimeSpan AS TS ON TT.TimeSpanId = TS.Id
+Inner Join dbo.[Doctor] AS D ON D.Id = TT.DoctorId
+Left Join dbo.TimeRecording AS TR ON TR.TimeTableId = TT.Id
+where  TR.Occupied = 0
