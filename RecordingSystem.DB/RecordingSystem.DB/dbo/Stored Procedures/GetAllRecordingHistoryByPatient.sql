ae9e0f22fa661a03e12a6dc1a9dcc152b84107a4
Vlad <VIStrelnikov94@gmail.com>
2023-02-01 23:33:13 +0300

    Add new procedure GetAllRecordingHistoryByPatient

---
+++ RecordingSystem.DB/RecordingSystem.DB/dbo/Stored Procedures/GetAllRecordingHistoryByPatient.sql
@@ -0,0 +1,9 @@
+﻿CREATE PROCEDURE [dbo].[GetAllRecordingHistoryByPatient]
+@Id int
+AS 
+SELECT Ptt.Id, Ptt.Name AS PatientName, Ptt.LastName, RecHis.DiagnosisId, Dg.Name AS DiagnosisName, Dg.Recommendations
+FROM [dbo].[Patient] AS Ptt 
+INNER JOIN [dbo].ActiveRecording AS AcRec ON Ptt.Id = AcRec.PatientId
+INNER JOIN [dbo].RecordingHistory AS RecHis ON AcRec.Id = RecHis.ActiveRecordingId
+INNER JOIN [dbo].Diagnosis AS Dg ON RecHis.DiagnosisId = Dg.Id
+WHERE Ptt.id = @Id AND AcRec.Coming = 1
