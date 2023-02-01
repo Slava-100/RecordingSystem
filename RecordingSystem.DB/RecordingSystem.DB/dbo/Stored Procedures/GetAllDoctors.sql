a1425533fe97e299887068a8d51ea17b4ab2d672
andreymuskat <andrey.strel.2001@gmail.com>
2023-02-01 16:54:06 +0300

    Change procedure GetAllDoctors

--- RecordingSystem.DB/RecordingSystem.DB/dbo/Stored Procedures/GetAllDoctors.sql
+++ RecordingSystem.DB/RecordingSystem.DB/dbo/Stored Procedures/GetAllDoctors.sql
@@ -3,3 +3,4 @@ AS
 select D.Id AS DoctorId , D.Name , D.LastName , Sp.Id As SpecializationId, Sp.Name As Specialization, Cab.Number AS NumberCabinet from dbo.[Doctor] AS D
 LEFT JOIN dbo.Specialization AS Sp ON D.[SpecializationId] = Sp.[Id]
 LEFT JOIN dbo.Cabinet AS Cab ON  D.[CabinetId] = Cab.[Id]
+where D.IsDeleted = 0
