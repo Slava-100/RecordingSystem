205ca65836ac806796ca6bf817e25a30f5765521
Svyatoslav <svat.strel.2001@gmail.com>
2023-02-01 17:44:23 +0300

    Change pole Number (Unique)

--- RecordingSystem.DB/RecordingSystem.DB/dbo/Tables/Cabinet.sql
+++ RecordingSystem.DB/RecordingSystem.DB/dbo/Tables/Cabinet.sql
@@ -2,6 +2,7 @@
     [Id]             INT           IDENTITY (1, 1) NOT NULL,
     [Number]         INT           NOT NULL,
     [Status]         BIT           NULL,
-    PRIMARY KEY CLUSTERED ([Id] ASC)
+    PRIMARY KEY CLUSTERED ([Id] ASC),
+    UNIQUE NONCLUSTERED ([Number] ASC)
 );
 
