5865e67315e57ef8969e515cefbd6d1427d9ee41
Svyatoslav <svat.strel.2001@gmail.com>
2023-02-01 17:23:16 +0300

    Change cabinet table

--- RecordingSystem.DB/RecordingSystem.DB/dbo/Tables/Cabinet.sql
+++ RecordingSystem.DB/RecordingSystem.DB/dbo/Tables/Cabinet.sql
@@ -1,8 +1,7 @@
 ﻿CREATE TABLE [dbo].[Cabinet] (
     [Id]             INT           IDENTITY (1, 1) NOT NULL,
     [Number]         INT           NOT NULL,
-    [Status]         INT           NULL,
-    [Specialization] NVARCHAR (30) NULL,
+    [Status]         BIT           NULL,
     PRIMARY KEY CLUSTERED ([Id] ASC)
 );
 
