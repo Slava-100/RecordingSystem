721cf45b756a6e97476859b3819fbce20cd67eba
Vlad <VIStrelnikov94@gmail.com>
2023-02-01 23:59:10 +0300

    Add new procedure UpdateDoctorById

---
+++ RecordingSystem.DB/RecordingSystem.DB/dbo/Stored Procedures/UpdateDoctorById.sql
@@ -0,0 +1,22 @@
+﻿CREATE PROCEDURE [dbo].[UpdateDoctorById]
+	@Id int,
+	@Name nvarchar(20), 
+	@LastName nvarchar(30), 
+	@Male bit, 
+	@PhoneNumber nvarchar(10), 
+	@Email nvarchar(20), 
+	@SpecializationId int, 
+	@CabinetId int, 
+	@Birthday datetime2(7) 
+AS
+UPDATE Doctor
+SET
+	Name = @Name, 
+	LastName = @LastName, 
+	Male = @Male, 
+	PhoneNumber = @PhoneNumber, 
+	Email = @Email, 
+	SpecializationId = @SpecializationId, 
+	CabinetId = @CabinetId, 
+	Birthday = @Birthday  
+WHERE Doctor.Id = @Id
