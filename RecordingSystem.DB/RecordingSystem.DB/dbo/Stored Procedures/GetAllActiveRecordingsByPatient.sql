30d8179c223b593e7db5505899e403a4c1690777
Svyatoslav <svat.strel.2001@gmail.com>
2023-02-01 17:13:39 +0300

    Add new procedure GetAllActiveRecordingsByPatient

---
+++ RecordingSystem.DB/RecordingSystem.DB/dbo/Stored Procedures/GetAllActiveRecordingsByPatient.sql
@@ -0,0 +1,8 @@
+﻿create procedure GetAllActiveRecordingsByPatient
+@Id_Patient Int
+AS
+select P.Id AS PatientId, P.Name AS PatientName, P.LastName, A.Id AS ActiveRecordingId, A.DateTime, A.Coming, D.Name AS DoctorName, D.LastName, C.Id AS CabinetId, C.Number  from dbo.Patient AS P
+Inner Join dbo.ActiveRecording AS A ON P.Id = A.PatientId
+Inner Join dbo.Doctor AS D ON D.Id = A.DoctorId
+Left Join dbo.Cabinet AS C On D.CabinetId = C.Id
+where P.Id = @Id_Patient AND A.Coming is NULL
