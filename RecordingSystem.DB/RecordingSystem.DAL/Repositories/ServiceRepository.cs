8c409473684c070a745c1675e6d713cb587a2533
Svyatoslav <svat.strel.2001@gmail.com>
2023-02-01 19:17:12 +0300

    Add new method AddService in DAL

---
+++ RecordingSystem.DB/RecordingSystem.DAL/Repositories/ServiceRepository.cs
@@ -0,0 +1,24 @@
+﻿using RecordingSystem.DAL.Models;
+using Dapper;
+using Microsoft.Data.SqlClient;
+using System.Data;
+
+
+namespace RecordingSystem.DAL.Repositories
+{
+    public class ServiceRepository
+    {
+        public void AddService(string name, float price, int specializationId, bool? male)
+        {
+            using (var sqlConnection = new SqlConnection(Options.sqlConnection))
+            {
+                sqlConnection.Open();
+
+                sqlConnection.Execute(StoredNamesProcedures.AddService,
+                    new { name, price, specializationId, male },
+                    commandType: CommandType.StoredProcedure);
+            }
+        }
+
+    }
+}
