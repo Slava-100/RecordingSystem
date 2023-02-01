bee8c4dea19f911edaf085d805c2e6474b922981
andreymuskat <andrey.strel.2001@gmail.com>
2023-02-01 17:00:19 +0300

    Add new procedure GetAllCabinets

---
+++ RecordingSystem.DB/RecordingSystem.DB/dbo/Stored Procedures/GetAllCabinets.sql
@@ -0,0 +1,5 @@
+﻿CREATE PROCEDURE [dbo].[GetAllCabinets]
+AS
+select D.CabinetId As CabinetId, Cabinet.Number AS CabinetNumber from dbo.[Doctor] AS D
+LEFT JOIN dbo.Cabinet ON D.[CabinetId] = Cabinet.[Id]
+where D.IsDeleted = 0
