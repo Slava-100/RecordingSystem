53d4d92cd4d19eb5aeda484f6462938a5a5bde51
Svyatoslav <svat.strel.2001@gmail.com>
2023-02-01 19:00:07 +0300

    Add method AddCabinet in CabinetRepository

---
+++ RecordingSystem.DB/RecordingSystem.DAL/Repositories/CabinetRepository.cs
@@ -0,0 +1,22 @@
+﻿using RecordingSystem.DAL.Models;
+using Dapper;
+using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace RecordingSystem.DAL.Repositories
+{
+    public class CabinetRepository
+    {
+        public void AddCabinet(int number, bool? status)
+        {
+            using (var sqlConnection = new SqlConnection(Options.sqlConnection))
+            {
+                sqlConnection.Open();
+
+                sqlConnection.Execute(StoredNamesProcedures.AddCabinet,
+                    new { number, status },
+                    commandType: CommandType.StoredProcedure);
+            }
+        }
+    }
+}
