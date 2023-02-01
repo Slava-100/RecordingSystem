2bd01b9e0c39eecb723a0725d55ad0848c120fac
Svyatoslav <svat.strel.2001@gmail.com>
2023-02-01 17:12:52 +0300

    Add new procedure GettAllPatientsByStatus

---
+++ RecordingSystem.DB/RecordingSystem.DB/dbo/Stored Procedures/GetAllPatientsByStatus.sql
@@ -0,0 +1,6 @@
+﻿create procedure GetAllPatientsByStatus
+@Id_Status INT
+AS
+select S.Id AS StatusId,S.Name AS StatusName, P.Id AS PatientId, P.Name AS PatientName, P.LastName, P.Male, P.Birthday from dbo.Patient AS P
+Left Join dbo.Status AS S ON S.Id = P.StatusId
+where P.IsDeleted = 0 AND @Id_Status = S.Id
