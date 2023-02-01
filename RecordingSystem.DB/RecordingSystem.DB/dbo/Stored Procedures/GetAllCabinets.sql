412d8511487faca94acc0ae38b51293e7178bcce
Svyatoslav <svat.strel.2001@gmail.com>
2023-02-01 21:53:37 +0300

    Change procedure GetAllCabinets

--- RecordingSystem.DB/RecordingSystem.DB/dbo/Stored Procedures/GetAllCabinets.sql
+++ RecordingSystem.DB/RecordingSystem.DB/dbo/Stored Procedures/GetAllCabinets.sql
@@ -1,3 +1,3 @@
 ﻿CREATE PROCEDURE [dbo].[GetAllCabinets]
 AS
-select C.Id As CabinetId, C.Number AS CabinetNumber from dbo.Cabinet AS C
+select C.Id As CabinetId, C.Number AS CabinetNumber, C.Status from dbo.Cabinet AS C
