c2690c51228059e7ce5ca6a2bdcf5a2305574de4
Vlad <VIStrelnikov94@gmail.com>
2023-02-02 17:04:57 +0300

    Add new method GetAllServiceByDoctor in DoctorRepository

--- RecordingSystem.DB/RecordingSystem.DAL/Repositories/DoctorRepository.cs
+++ RecordingSystem.DB/RecordingSystem.DAL/Repositories/DoctorRepository.cs
@@ -16,5 +16,38 @@ namespace RecordingSystem.DAL.Repositories
                     commandType: CommandType.StoredProcedure).ToList();
             }
         }
+        public List<ServiceDto> GetAllDoctorByServices(int id)
+        {
+            using (var sqlConnection = new SqlConnection(Options.sqlConnection))
+            {
+                List<ServiceDto> result = new List<ServiceDto>();
+
+                sqlConnection.Open();
+                return sqlConnection.Query < ServiceDto, DoctorDto, ServiceDto>(
+                    StoredNamesProcedures.GetAllServiceByDoctor,
+                    (service, doctor) => 
+                    {
+                        ServiceDto crnt = null;
+                        foreach (var tmpS in result)
+                        {
+                            if(tmpS.Id == service.Id)
+                            {
+                                crnt = tmpS;
+                            }
+                        }
+                        if (crnt is null)
+                        {
+                            crnt = service;
+                            result.Add(crnt);
+                        }
+                        return service;
+                    },
+                    new { id },
+                    splitOn: "Id",
+                    commandType: CommandType.StoredProcedure).ToList();
+
+                return result;
+            }
+        }
     }
 }
