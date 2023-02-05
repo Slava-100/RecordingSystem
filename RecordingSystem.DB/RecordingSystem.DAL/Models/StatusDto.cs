a143e6f22b7c02a69c8fbcb874b5136fee4b671e
Svyatoslav <svat.strel.2001@gmail.com>
2023-02-05 14:50:56 +0300

    Add GetPatientsByStatus

---
+++ RecordingSystem.DB/RecordingSystem.DAL/Models/StatusDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecordingSystem.DAL.Models
+{
+    public class StatusDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int? Discount { get; set; }
+    }
+}
