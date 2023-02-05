1d5f376ef5a48eaa8ee793bc82d64cd545535a27
Svyatoslav <svat.strel.2001@gmail.com>
2023-02-06 00:13:19 +0300

    Add new method GetTimeTableByDoctorId

--- RecordingSystem.DB/RecordingSystem.DAL/Repositories/StoredNamesProcedures.cs
+++ RecordingSystem.DB/RecordingSystem.DAL/Repositories/StoredNamesProcedures.cs
@@ -32,6 +32,7 @@ namespace RecordingSystem.DAL.Repositories
         public const string GetAllPatientsByStatusId = "GetAllPatientsByStatusId";
         public const string GetAllServiceByDoctorId = "GetAllServiceByDoctorId";
         public const string GetAllDoctorsByServiceId = "GetAllDoctorsByServiceId";
-        public const string GetAllActiveRecordingsByPatientId = "GetAllActiveRecordingsByPatientId";
+        public const string GetAllActiveRecordingsByPatientId = "GetAllActiveRecordingsByPatientId"; 
+        public const string GetTimeTableByDoctorId = "GetTimeTableByDoctorId";
     }
 }
---
+++ RecordingSystem.DB/RecordingSystem.DAL/Repositories/TimeTableRepository.cs
@@ -0,0 +1,32 @@
+﻿using Dapper;
+using Microsoft.Data.SqlClient;
+using RecordingSystem.DAL.Models;
+using System.Data;
+
+namespace RecordingSystem.DAL.Repositories
+{
+    public class TimeTableRepository
+    {
+        public List<TimeTableDto> GetTimeTableByDoctorId(int Id)
+        {
+            using (var sqlConnection = new SqlConnection(Options.sqlConnection))
+            {
+                sqlConnection.Open();
+                return sqlConnection.Query<TimeTableDto, DoctorDto,SpecializationDto,CabinetDto,DayOfWeekDto,TimeSpanDto,TimeTableDto>(StoredNamesProcedures.GetTimeTableByDoctorId,
+                    (timeTable, doctor, specialization, cabinet, dayOfWeek, timeSpan) =>
+                    {
+                        timeTable.Doctor = doctor;
+                        doctor.Specialization = specialization;
+                        doctor.Cabinet = cabinet;
+                        timeTable.DayOfWeek = dayOfWeek;
+                        timeTable.TimeSpan = timeSpan;
+
+                        return timeTable;
+                    },
+                    new { Id },
+                    splitOn: "Id",
+                    commandType: CommandType.StoredProcedure).ToList();
+            }
+        }
+    }
+}
