ead65d0a3fd90081b84400e0c2bd048e249ea912
andreymuskat <andrey.strel.2001@gmail.com>
2023-02-07 01:18:48 +0300

    add DoctorOutputModel

---
+++ RecordingSystem.DB/RecordingSytem.BLL/Models/DoctorOutputModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecordingSystem.BLL.Models
+{
+    public class DoctorOutputModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public int? SpecializationId { get; set; }
+        public int? CabinetId { get; set; }
+    }
+}
