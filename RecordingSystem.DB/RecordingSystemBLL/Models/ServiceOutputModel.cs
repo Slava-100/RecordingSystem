2907c69aececc203d7cae0c5b4ad65afb4461173
Vlad <VIStrelnikov94@gmail.com>
2023-02-07 15:41:55 +0300

    add bll

---
+++ RecordingSystem.DB/RecordingSystemBLL/Models/ServiceOutputModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecordingSystem.BLL.Models
+{
+    public class ServiceOutputModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public float? Price { get; set; }
+        public int? SpecializationId { get; set; }
+        public bool? IsDeleted { get; set; }
+        public bool? Male { get; set; }
+    }
+}
---
+++ RecordingSystem.DB/RecordingSystemBLL/ServiceManager.cs
@@ -0,0 +1,24 @@
+﻿using RecordingSystem.BLL.Models;
+using RecordingSystem.DAL.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecordingSystem.BLL
+{
+    public class ServiceManager
+    {
+        Mapperrr _mapperrr = new Mapperrr();
+        public List<ServiceOutputModel> GetAllProducts()
+        {
+            ServiceRepository repository = new ServiceRepository();
+            var products = repository.AddService();
+
+            var result = _mapperrr.MapServiceDtoToProductOutputModel(products);
+
+            return result;
+        }
+    }
+}
