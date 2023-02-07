bd5e217bf39723b16da8f70f6ed5e9c335443e0c
Vlad <VIStrelnikov94@gmail.com>
2023-02-07 08:49:20 +0300

    add DiagnosisDto, RecordingHistoryDto

---
+++ RecordingSystem.DB/RecordingSystem.DAL/Models/RecordingHistoryDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecordingSystem.DAL.Models
+{
+    internal class RecordingHistoryDto
+    {
+        public string Name { get; set; }
+        public string Recommendations { get; set; }
+    }
+}
