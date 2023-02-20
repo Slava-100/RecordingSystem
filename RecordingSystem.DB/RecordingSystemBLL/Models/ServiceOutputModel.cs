40974abd1ca931133794d08e744f7cb7d6236a6d
Vlad <VIStrelnikov94@gmail.com>
2023-02-21 01:28:45 +0300

    add GetAllServiceByMaleTest

--- RecordingSystem.DB/RecordingSystemBLL/Models/ServiceOutputModel.cs
+++ RecordingSystem.DB/RecordingSystemBLL/Models/ServiceOutputModel.cs
@@ -1,4 +1,5 @@
 ﻿using RecordingSystem.DAL.Models;
+
 namespace RecordingSystem.BLL.Models
 {
     public class ServiceOutputModel
@@ -10,5 +11,17 @@ namespace RecordingSystem.BLL.Models
         public bool? IsDeleted { get; set; }
         public bool? Male { get; set; }
         public SpecializationDto Specialization { get; set; }
+
+        public override bool Equals(object? obj)
+        {
+            return obj is ServiceOutputModel model &&
+                   Id == model.Id &&
+                   Name == model.Name &&
+                   Price == model.Price &&
+                   SpecializationId == model.SpecializationId &&
+                   IsDeleted == model.IsDeleted &&
+                   Male == model.Male &&
+                   Specialization.Equals(model.Specialization);
+        }
     }
 }
