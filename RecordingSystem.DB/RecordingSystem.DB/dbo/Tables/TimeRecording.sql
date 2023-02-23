2a900902473f210c2e1fc667944ba6e54eb6ba40
Svyatoslav <svat.strel.2001@gmail.com>
2023-02-23 03:25:23 +0300

    Fix

--- RecordingSystem.DB/RecordingSystem.DB/dbo/Tables/TimeRecording.sql
+++ RecordingSystem.DB/RecordingSystem.DB/dbo/Tables/TimeRecording.sql
@@ -1,7 +1,7 @@
 ﻿CREATE TABLE [dbo].[TimeRecording] (
     [Id]          INT  IDENTITY (1, 1) NOT NULL,
     [Date]        datetime2(7) NOT NULL,
-    [TimeTableId] INT  NULL,
+    [TimeTableId] INT  NOT NULL,
     [Occupied]    BIT  DEFAULT ((0)) NOT NULL,
     PRIMARY KEY CLUSTERED ([Id] ASC),
     FOREIGN KEY ([TimeTableId]) REFERENCES [dbo].[TimeTable] ([Id])
