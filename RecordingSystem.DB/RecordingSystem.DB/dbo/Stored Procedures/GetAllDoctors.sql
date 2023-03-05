881e07270e3dbf554669f075d6548d9941d31fd7
Svyatoslav <svat.strel.2001@gmail.com>
2023-03-05 18:15:11 +0300

    change GetAllDoctors

--- RecordingSystem.DB/RecordingSystem.DB/dbo/Stored Procedures/GetAllDoctors.sql
+++ RecordingSystem.DB/RecordingSystem.DB/dbo/Stored Procedures/GetAllDoctors.sql
@@ -1,6 +1,6 @@
 ﻿CREATE PROCEDURE [dbo].[GetAllDoctors]
 AS
-select D.Id, D.Name, D.LastName, D.Male, D.Birthday, Sp.Id, Sp.Name, Cab.Id, Cab.Number from dbo.[Doctor] AS D
+select D.Id, D.Name, D.LastName, D.Male, D.Birthday, D.Email, D.PhoneNumber, Sp.Id, Sp.Name, Cab.Id, Cab.Number from dbo.[Doctor] AS D
 LEFT JOIN dbo.Specialization AS Sp ON D.[SpecializationId] = Sp.[Id]
 LEFT JOIN dbo.Cabinet AS Cab ON  D.[CabinetId] = Cab.[Id]
 where D.IsDeleted = 0
