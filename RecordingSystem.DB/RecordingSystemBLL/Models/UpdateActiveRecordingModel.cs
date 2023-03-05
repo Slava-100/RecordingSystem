d442e9d79ca3db995cddcf0515382693932ae30a
Svyatoslav <svat.strel.2001@gmail.com>
2023-03-06 01:25:17 +0300

    Add ActiveRecording

--- RecordingSystem.DB/RecordingSystemBLL/Models/UpdateActiveRecordingModel.cs
+++ RecordingSystem.DB/RecordingSystemBLL/Models/UpdateActiveRecordingModel.cs
@@ -10,12 +10,14 @@ namespace RecordingSystem.BLL.Models
     {
         public int? Id { get; set; }
         public bool? Coming { get; set; }
+        public bool? IsDeleted  { get; set; }
 
         public override bool Equals(object? obj)
         {
             return obj is UpdateActiveRecordingModel model &&
                    Id == model.Id &&
-                   Coming == model.Coming;
+                   Coming == model.Coming && 
+                   IsDeleted == model.IsDeleted;
         }
     }
 }
