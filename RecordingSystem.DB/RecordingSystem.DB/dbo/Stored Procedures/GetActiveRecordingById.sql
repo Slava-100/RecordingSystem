7b4b6bf9fd735c3c847340a41cd15d81eb5a8dd0
Svyatoslav <svat.strel.2001@gmail.com>
2023-03-06 15:12:33 +0300

    GetActiveRecordingById

---
+++ RecordingSystem.DB/RecordingSystem.DB/dbo/Stored Procedures/GetActiveRecordingById.sql
@@ -0,0 +1,7 @@
+﻿create procedure dbo.GetActiveRecordingById
+@Id int
+AS
+select*from dbo.ActiveRecording AS A
+Inner Join dbo.Patient AS P On A.PatientId = P.Id 
+Inner Join dbo.Doctor AS D ON D.Id = A.DoctorId
+where @Id = A.Id
